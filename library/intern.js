1f9cb83b81c5278593cefb38c073059d7b997578
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2022-12-16 12:17:38 -0500

    added testing for employee.test.js

--- library/intern.js
+++ library/intern.js
@@ -1,5 +1,7 @@
+const Employee = require('./employee')
+
 class Intern extends Employee {
-    constructor(name, id, email, github) {
+    constructor(name, id, email, school) {
         super(name, id, email)
         this.school = school
     }
